4add919460cd576b6f5fa9aad17bc784b4333dda
Valid Rabiee <validrabieejs@gmail.com>
2023-08-30 19:12:29 +0330

    adding normal javascript logic to components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,18 @@
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  // برای تاریخ فارسی fa-IR 
+  const month = props.date.toLocaleString('en-US', {month: 'long'});
+  const year = props.date.getFullYear();
+  // expenseDate.toLocaleString('en-US', {year: 'numeric'})
+  const day = props.date.toLocaleString('en-US', {day: '2-digit'});
 
   return ( 
     <div className='expense-item'>
       <div className='expense-date'>
-        {/* برای تاریخ فارسی fa-IR */}
-        <div className='expense-date__month'>{props.date.toLocaleString('en-US', {month: 'long'})}</div>
-        <div className='expense-date__year'>{props.date.getFullYear()}</div>
-        {/* expenseDate.toLocaleString('en-US', {year: 'numeric'}) */}
-        <div className='expense-date__day'>{props.date.toLocaleString('en-US', {day: '2-digit'})}</div>
+        <div className='expense-date__month'>{month}</div>
+        <div className='expense-date__year'>{year}</div>
+        <div className='expense-date__day'>{day}</div>
       </div>
       <div className='expense-item__description'>
         <h2 className='expense-item__title'>{props.title}</h2>
