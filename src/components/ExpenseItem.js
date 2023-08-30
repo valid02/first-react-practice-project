de8f6e6c8eb8ec0372d1aa50480ca75812d2cb82
Valid Rabiee <validrabieejs@gmail.com>
2023-08-30 17:27:12 +0330

    props & passing data to component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,22 +1,19 @@
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
-  const expenseDate = new Date(2002, 6, 29);
-  const expenseTitle = 'New TV';
-  const expenseAmount = 799.45;
+const ExpenseItem = (props) => {
 
   return ( 
     <div className='expense-item'>
       <div className='expense-date'>
         {/* برای تاریخ فارسی fa-IR */}
-        <div className='expense-date__month'>{expenseDate.toLocaleString('en-US', {month: 'long'})}</div>
-        <div className='expense-date__year'>{expenseDate.getFullYear()}</div>
+        <div className='expense-date__month'>{props.date.toLocaleString('en-US', {month: 'long'})}</div>
+        <div className='expense-date__year'>{props.date.getFullYear()}</div>
         {/* expenseDate.toLocaleString('en-US', {year: 'numeric'}) */}
-        <div className='expense-date__day'>{expenseDate.toLocaleString('en-US', {day: '2-digit'})}</div>
+        <div className='expense-date__day'>{props.date.toLocaleString('en-US', {day: '2-digit'})}</div>
       </div>
       <div className='expense-item__description'>
-        <h2 className='expense-item__title'>{expenseTitle}</h2>
-        <div className='expense-item__price'>${expenseAmount}</div>
+        <h2 className='expense-item__title'>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
    );
