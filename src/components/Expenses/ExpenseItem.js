64076d823ef99458d3bcd9055d806c23f8b30705
Valid Rabiee <validrabieejs@gmail.com>
2023-09-01 14:38:20 +0330

    Form  inputs & multiple states & two way binding

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,16 @@
-import { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle('updated!');
-    
-    console.log(title);
-  }
 
   return ( 
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2 className='expense-item__title'>{title}</h2>
+        <h2 className='expense-item__title'>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>change title</button>
     </Card>
    );
 }
